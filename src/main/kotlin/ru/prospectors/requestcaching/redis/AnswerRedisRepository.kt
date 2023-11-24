a326ff0bcc7626a5b6b3a8b3df45a8154a00cd96
VladimirS01 <vbnm1993@yandex.ru>
2023-11-24 10:02:57 +0500

    version 1.2

--- src/main/kotlin/ru/prospectors/requestcaching/redis/AnswerRedisRepository.kt
+++ src/main/kotlin/ru/prospectors/requestcaching/redis/AnswerRedisRepository.kt
@@ -3,6 +3,7 @@ package ru.prospectors.requestcaching.redis
 import org.redisson.api.RMapCacheReactive
 import org.redisson.api.RedissonReactiveClient
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import ru.prospectors.requestcaching.model.Answer
@@ -10,6 +11,7 @@ import ru.prospectors.requestcaching.repository.AnswerRepository
 import ru.prospectors.requestcaching.webclient.WebClientRepository
 import java.util.concurrent.TimeUnit
 
+@Primary
 @Repository
 class AnswerRedisRepository(
     private val client: RedissonReactiveClient,
@@ -21,15 +23,7 @@ class AnswerRedisRepository(
 
     override fun findById(id: String): Mono<Answer?> {
         return answers.get(id)
-            .switchIfEmpty(webClient.findById(id).flatMap { answer ->
-                if (answer != null) {
-                    save(answer)
-                } else {
-                    Mono.empty()
-                }
-            })
     }
-
 
     override fun save(answer: Answer): Mono<Answer> {
         return answers.fastPut(answer.id, answer, ttl, TimeUnit.MINUTES)
--- src/main/kotlin/ru/prospectors/requestcaching/service/DomainAnswerService.kt
+++ src/main/kotlin/ru/prospectors/requestcaching/service/DomainAnswerService.kt
@@ -1,16 +1,28 @@
 package ru.prospectors.requestcaching.service
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import ru.prospectors.requestcaching.model.Answer
 import ru.prospectors.requestcaching.repository.AnswerRepository
+import ru.prospectors.requestcaching.webclient.WebClientRepository
 
 @Primary
 @Service
 class DomainAnswerService(
-    @Qualifier("answerRedisRepository") private val answerRepository: AnswerRepository
+    private val answerRepository: AnswerRepository,
+    private val webClient: WebClientRepository
 ): AnswerService {
-    override fun findById(id: String) = answerRepository.findById(id)
+    override fun findById(id: String): Mono<Answer?> {
+        return answerRepository.findById(id)
+            .switchIfEmpty(webClient.findById(id)).flatMap { answer ->
+                if (answer != null) {
+                    addAnswer(answer)
+                } else {
+                    Mono.empty()
+                }
+            }
+    }
+
     override fun addAnswer(answer: Answer) = answerRepository.save(answer)
 }
